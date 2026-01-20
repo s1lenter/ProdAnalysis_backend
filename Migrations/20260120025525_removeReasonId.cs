6fdc20d02c6fbe1685b16e3c3aa9f734c71a153d
s1lenter <sega29600@gmail.com>
2026-01-20 07:58:37 +0500

    Добавить миграции

---
+++ Migrations/20260120025525_removeReasonId.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ProductionAnalysisBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeReasonId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deviations_Reasons_ReasonId",
+                table: "Deviations");
+
+            migrationBuilder.DropTable(
+                name: "Reasons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deviations_ReasonId",
+                table: "Deviations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reasons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GroupId = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reasons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reasons_ReasonGroups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "ReasonGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deviations_ReasonId",
+                table: "Deviations",
+                column: "ReasonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reasons_GroupId",
+                table: "Reasons",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deviations_Reasons_ReasonId",
+                table: "Deviations",
+                column: "ReasonId",
+                principalTable: "Reasons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ Migrations/20260120025654_removeReasonIdFromDeviation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductionAnalysisBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeReasonIdFromDeviation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReasonId",
+                table: "Deviations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReasonId",
+                table: "Deviations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
