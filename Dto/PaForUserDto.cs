d47a452ada77c2c154b566ea13dcf28819848c21
s1lenter <sega29600@gmail.com>
2026-01-20 08:36:35 +0500

    Добавить дто

---
+++ Dto/PaForUserDto.cs
@@ -0,0 +1,8 @@
+namespace ProductionAnalysisBackend.Dto;
+
+public class PaForUserDto
+{
+    public int ProductionAnalysisId  { get; set; }
+    public int UserId { get; set; }
+    public string Scenario { get; set; }
+}
